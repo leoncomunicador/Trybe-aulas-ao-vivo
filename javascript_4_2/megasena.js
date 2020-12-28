3cef5ae9d69e42d35c2f2bcd4673b2a479001774
Daniel Leon <daniel.radialista@gmail.com>
2020-12-28 14:45:00 -0300

    gerando os numeros de forma aleatoria

---
+++ javascript_4_2/megasena.js
@@ -0,0 +1,13 @@
+// Ter um array com os números da mega sena
+let megaSenaNumbers = [];
+
+let firstNumber = Math.ceil(Math.random() * 60);
+let secondNumber = Math.ceil(Math.random() * 60);
+let thirdNumber = Math.ceil(Math.random() * 60);
+let fourthNumber = Math.ceil(Math.random() * 60);
+let fifthNumber = Math.ceil(Math.random() * 60);
+let sixthNumber = Math.ceil(Math.random() * 60);
+
+megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
+
+console.log(megaSenaNumbers);
