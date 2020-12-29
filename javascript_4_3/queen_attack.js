f4e2b671e9b1f12cda7a141773198c0827a1b0ca
Daniel Leon <daniel.radialista@gmail.com>
2020-12-29 15:09:47 -0300

    Verifica vertical, horizontal e diagonal superior direita

---
+++ javascript_4_3/queen_attack.js
@@ -0,0 +1,30 @@
+// Guardar a posição da Rainha
+let posicaoRainhaLinha = 4;
+let posicaoRainhaColuna = 4;
+
+// Guarda a posição da Peça
+let posicaoPecaLinha = 7;
+let posicaoPecaColuna = 7;
+
+let ataqueBemSucedido = false;
+
+if (posicaoRainhaLinha === posicaoPecaLinha || posicaoRainhaColuna === posicaoPecaColuna) {
+    ataqueBemSucedido = true;
+}
+
+// Diagonal superior direita
+//4 x 4
+for (let i = 1; i < 8; i += 1) {
+    let linhaRainha = posicaoRainhaLinha + i;
+    let colunaRainha = posicaoRainhaColuna + i;
+
+    if (linhaRainha > 8 || colunaRainha > 8) {
+        break;
+    }
+
+    if (linhaRainha === posicaoPecaLinha && colunaRainha === posicaoPecaColuna) {
+        ataqueBemSucedido = true;
+    }
+}
+
+console.log(ataqueBemSucedido);
